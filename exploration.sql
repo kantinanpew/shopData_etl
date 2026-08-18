ec635d0e5c7b232a1e686cb3b82c91bbdc13c4c6
kantinan3820 <kantinan.pew@gmail.com>
2026-08-18 17:19:38 +0700

    check missing contact and phone no. formats

--- exploration.sql
+++ exploration.sql
@@ -4,7 +4,7 @@ SELECT 'vw_raw_customers' AS source, COUNT(*)  AS rows FROM vw_raw_customers
 UNION ALL SELECT 'vw_raw_orders', COUNT(*) FROM vw_raw_orders
 UNION ALL SELECT 'vw_exchange_rates', COUNT(*) FROM vw_exchange_rates;
 
--- check duplicated customer id
+-- check duplicated customer
 
 SELECT customer_id, COUNT(*) as occurrences,
     GROUP_CONCAT(signup_date, '|' ) AS signup_date,
@@ -12,3 +12,20 @@ SELECT customer_id, COUNT(*) as occurrences,
 FROM vw_raw_customers
 GROUP BY customer_id
 HAVING COUNT(*) >1;
+
+-- check missing contact info Null etc...
+
+SELECT customer_id, full_name, email, phone
+FROM vw_raw_customers
+WHERE  email IS NULL OR TRIM(email) = ''
+OR email IS NULL OR TRIM(phone) = '';
+
+-- check the phone no. formats
+
+SELECT customer_id, phone,
+    CASE 
+        WHEN phone GLOB '*[A-Za-z]*' THEN 'contains letters'
+        WHEN phone GLOB '*[^0-9]' THEN 'contains symbols/ spaces'
+    END AS issue
+FROM vw_raw_customers
+WHERE phone IS NOT NULL AND phone GLOB '*[^0-9]';
